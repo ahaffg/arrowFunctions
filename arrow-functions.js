1342fe124e74b93c3dfc1fe8fe684c908440573a
ahaffg <angharad-fflur@live.co.uk>
2022-08-05 20:10:11 +0000

    arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,8 +4,19 @@
  */
 
 // Vanilla JavaScript Function
-
+function addTwoNumbers(a,b){
+//code block 
+return a + b;
+}
+sum = addTwoNumbers (3,5);
+console.log(sum); 
 // Arrow Function With Parameters
+function addTwoNumbers(a,b){
+    //code block
+    return a + b;
+    }
+    sum = addTwoNumbers (3,5);
+    console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
